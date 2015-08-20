3b76ee66c9aac308f2159689f790d010ec7fa995
Max - GreyFawkes <mvonthaden@gmail.com>
2015-08-20 18:30:56 -0400

    Assignment 12 completed

--- foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
+++ foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
@@ -1,20 +1,20 @@
 
 package com.bloc.syntax;
 
-public class SyntaxDisaster extends Object {
+public class SyntaxDisaster{
 
 	/************************************************
 	 *	ASSIGNMENT:
 	 *	Fix the code found below this block
 	/************************************************/
 
-	public static void main(String [] args) {;}
-		if (true); {
-			int x = 5
+	public static void main(String[] args) {
+		if (true) {
+			int x = 5;
 		}
-		int syntaxIsFun; 5;
-		/* The following line prints stuff out, I promise
-		System.out.println("If you can see this message, you've won!);
+		int syntaxIsFun = 5;
+		// The following line prints stuff out, I promise
+		System.out.println("If you can see this message, you've won!");
 	}
 
 	/************************************************
