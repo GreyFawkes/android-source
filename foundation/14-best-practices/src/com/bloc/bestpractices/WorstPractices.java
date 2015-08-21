fefde43026b7108c010028339832afe24d8da328
Max - GreyFawkes <mvonthaden@gmail.com>
2015-08-21 14:07:27 -0400

    assignment 14 complete

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -1,41 +1,39 @@
 package com.bloc.bestpractices;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class WorstPractices extends Object {
+public class WorstPractices {
 
 	/************************************************
 	 *	ASSIGNMENT:
 	 *	Fix code and comments below this block
 	/************************************************/
 
-	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+	public static void main(String[] args) {
+		int magicNumber = WorstPractices.magicNumberGenerator(false);
+		magicNumber *= 5;
+		if (magicNumber > 18) {
+			while(magicNumber > 0){
+			magicNumber--;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
+// magicNumberGenerator
+// this method takes in a single parameter, isTrue. 
+// It calculates a magic number and 
 // returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
-}
+// a seed which helps generate the magic number
+	private static int magicNumberGenerator(boolean isTrue){
+		
+			//Start off with one of these
+		int magicInt = isTrue ? 34 : 21;
+		float magicFloat = 0.5f;
+		
+		for (int i = 0; i < magicInt; i++) { 
+			magicFloat *= magicInt;
+		} 
+	
+		return (int)(magicFloat * magicInt);
+	}
 
 	/************************************************
 	 *	ASSIGNMENT
