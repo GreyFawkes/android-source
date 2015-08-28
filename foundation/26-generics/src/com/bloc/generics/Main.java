07203018d5edcaca3b3824ea7eeb8b69b926cb57
Max - GreyFawkes <mvonthaden@gmail.com>
2015-08-27 22:11:30 -0400

    assignment-26 complete

--- foundation/26-generics/src/com/bloc/generics/Main.java
+++ foundation/26-generics/src/com/bloc/generics/Main.java
@@ -12,6 +12,14 @@ public class Main extends Object {
  		 *	ASSIGNMENT:
  		 *	Place several Toy objects into toyBox
 		/************************************************/
+                
+                Toy<ActionFigure> actionFigure = new Toy(new ActionFigure());
+                Toy<Book> book = new Toy(new Book());
+                Toy<Spoon> spoon = new Toy(new Spoon());
+                
+                toyBox.addToy(actionFigure);
+                toyBox.addToy(book);
+                toyBox.addToy(spoon);
 
 		assert toyBox.getToyCount() > 0 : "Let's get some toys in that box!";
 		System.out.println("Inside your toybox you've got:");
